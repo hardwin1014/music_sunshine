678e598119300442f14955668a6144f30db61c04
codewxh <codewxh@users.noreply.github.com>
2022-05-10 09:46:17 +0000

    Update Vue3.x ed0a443815c91d0b0d55a4450c8943c628d8a99f

---
+++ js/about.4c891bca.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmusic_sunshine"]=self["webpackChunkmusic_sunshine"]||[]).push([[443],{7381:function(n,s,u){u.r(s),u.d(s,{default:function(){return f}});var e=u(3396);const i={class:"about"},t=(0,e._)("h1",null,"This is an about page",-1),a=[t];function c(n,s){return(0,e.wg)(),(0,e.iD)("div",i,a)}var r=u(89);const h={},o=(0,r.Z)(h,[["render",c]]);var f=o}}]);
+//# sourceMappingURL=about.4c891bca.js.map
