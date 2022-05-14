a9bdcb6751913e2d0eddd9475f969f3404b07554
codewxh <914067438@qq.com>
2022-05-14 23:13:27 +0800

    chore

--- vue.config.js
+++ vue.config.js
@@ -22,20 +22,6 @@ module.exports = {
     // app是express的一个实例
     before(app) {
       registerRouter(app)
-    },
-    host: '127.0.0.1', // 本地地址
-    port: 8084, // 端口号
-    open: true, // 配置项目在启动时自动在浏览器打开
-    proxy: {
-      '/music-next/': {
-        // '/api'是代理标识，一般是每个接口前的相同部分
-        target: 'http://ustbhuangyi.com', // 请求地址，一般是服务器地址
-        changeOrigin: true, // 是否进行跨域
-        pathRewrite: {
-          // pathRewrite的作用是把请求接口中的 '/api'替换掉，一般是替换为空""
-          '^/api': ''
-        }
-      }
     }
   },
   configureWebpack: (config) => {
